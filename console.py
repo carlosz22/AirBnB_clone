e080215a7b7a6c2ea6810b94daf7e5a861901da3
Carlos Zuluaga <carlosez9@hotmail.com>
2019-11-12 14:31:03 -0500

    Feature: Adding show, destroy, all and update in the console

--- console.py
+++ console.py
@@ -38,7 +38,6 @@ class HBNBCommand(cmd.Cmd):
             if line in HBNBCommand.cls_arr:
                 class_to_ins = HBNBCommand.cls_arr.get(line)
                 new_inst = class_to_ins()
-                # not working stuff to review
                 new_inst.save()
                 print(new_inst.id)
             else:
@@ -59,9 +58,8 @@ class HBNBCommand(cmd.Cmd):
                 models.storage.all().keys():
             print("** no instance found **")
         else:
-            print("Hello")
-            # models.storage.all().get(list_line[0] +
-            # '.' + list_line[1]).to_dict())
+            obj = models.storage.all().get(list_line[0] + '.' + list_line[1])
+            print(obj)
 
     def do_destroy(self, line):
         """Deletes an instance based on the class name and id"""
@@ -76,9 +74,48 @@ class HBNBCommand(cmd.Cmd):
                 models.storage.all().keys():
             print("** no instance found **")
         else:
-            # Add delete behavior
-            print("Delete key from dictionary")
-            # models.storage.all().get(list_line[0] + '.' + list_line[1]))
+            models.storage.all().pop(list_line[0] + '.' + list_line[1], None)
+            models.storage.save()
+
+    def do_all(self, line):
+        """Prints string repr of all instances based on the class name"""
+        list_line = line.split(' ')
+        string = ""
+        list_all = []
+        if line == "":
+            for key, value in models.storage.all().items():
+                string = str(value)
+                list_all.append(string)
+            print(list_all)
+        elif list_line[0] not in HBNBCommand.cls_arr.keys():
+            print("** class doesn't exist **")
+        else:
+            for key, value in models.storage.all().items():
+                if value.__class__.__name__ == list_line[0]:
+                    string = str(value)
+                    list_all.append(string)
+            print(list_all)
+
+    def do_update(self, line):
+        """Updates an instance attribute"""
+        list_line = line.split(' ')
+        if line == "":
+            print("** class name missing **")
+        elif list_line[0] not in HBNBCommand.cls_arr.keys():
+            print("** class doesn't exist **")
+        elif len(list_line) < 2:
+            print("** instance id missing **")
+        elif list_line[0] + '.' + list_line[1] not in \
+                models.storage.all().keys():
+            print("** no instance found **")
+        elif len(list_line) < 3:
+            print("** attribute name missing **")
+        elif len(list_line) < 4:
+            print("** value missing **")
+        else:
+            obj = models.storage.all().get(list_line[0] + '.' + list_line[1])
+            setattr(obj, list_line[2], list_line[3][1:-1])
+            obj.save()
 
 
 if __name__ == "__main__":
