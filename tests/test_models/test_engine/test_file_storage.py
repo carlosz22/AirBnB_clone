ca51c5fdcc8fde8cfdbccd7c842a1d1176761d37
Carlos Zuluaga <carlosez9@hotmail.com>
2019-11-13 14:18:30 -0500

    Adding more test to test_file_storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,13 +3,19 @@
 Unittest for FileStorage class
 """
 import unittest
-import models
 import pep8
+import os
 from models.engine.file_storage import FileStorage
 
 
 class TestFileStorage(unittest.TestCase):
     """ Test cases for FileStorage Class """
+
+    def tearDown(self):
+        """Deleting everything at the end"""
+        FileStorage._FileStorage__objects = {}
+        if os.path.exists(FileStorage._FileStorage__path_file):
+            os.remove(FileStorage._FileStorage__path_file)
 
     def test_pep8_conformance_model_files(self):
         """
@@ -33,6 +39,11 @@ class TestFileStorage(unittest.TestCase):
         """ Tests instance of a class """
         storage = FileStorage()
         self.assertTrue(isinstance(storage, FileStorage))
+
+    def test__file_path_exists(self):
+        """Tests for the file path existance"""
+        storage = FileStorage()
+        self.assertTrue(isinstance(storage._FileStorage__file_path, str))
 
     def test_all(self):
         """Tests all() function"""
